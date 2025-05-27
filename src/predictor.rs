0ef65040f1841b8ff508f6cc16a1f09f799b33d5
jkuya <kuyajesee@proton.me>
2025-05-27 04:29:50 +0300

    Move predict function to predictor module for better organization

---
+++ src/predictor.rs
@@ -0,0 +1,18 @@
+#[allow(dead_code)]
+pub fn predict(tree: &TreeNode, sample: &[u8]) -> f64 {
+    match tree {
+        TreeNode::Leaf { value } => *value,
+        TreeNode::Internal {
+            feature_index,
+            threshold_bin,
+            left,
+            right,
+        } => {
+            if sample[*feature_index] as usize <= *threshold_bin {
+                predict(left, sample)
+            } else {
+                predict(right, sample)
+            }
+        }
+    }
+}
