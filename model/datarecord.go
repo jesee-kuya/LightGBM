231db19763dc1b12553317d811ff9d3cae5fde02
jkuya <kuyajesee@proton.me>
2025-06-05 04:32:33 +0300

    feat: Add DataRecord struct to model package

---
+++ model/datarecord.go
@@ -0,0 +1,16 @@
+package model
+
+type DataRecord struct {
+	ID              string
+	County          string
+	HealthLevel     string
+	YearsExperience float64
+	Prompt          string
+	Competency      string
+	Panel           string
+	Clinician       string
+	GPT4            string
+	LLAMA           string
+	GEMINI          string
+	DDXSNOMED       string
+}
